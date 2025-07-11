68b59cb3c8dc53ba1b05499ae60e366c219c24cf
Asaf Shakarzy <asaf000@gmail.com>
2025-07-11 07:22:03 +0300

    chore(examples): add langgraph weather agent example
    resolves #74

---
+++ examples/utils/utils.py
@@ -0,0 +1,51 @@
+from typing import cast
+
+from flux0_core.agent_runners.api import Deps
+from flux0_core.agent_runners.context import Context
+from flux0_core.sessions import (
+    MessageEventData,
+    StatusEventData,
+)
+from flux0_stream.emitter.api import EventEmitter
+
+STATUS_EVENT_DATA_KEY = "detail"
+
+
+async def read_user_input(deps: Deps, context: Context) -> str:
+    """Extract user input from the last message event."""
+
+    # read session events and expect the last event to be the user input
+    user_input = None
+
+    events = await deps.list_session_events(context.session_id)
+    last_event = events[-1]
+    if last_event.type != "message":
+        raise ValueError(f"Expected last event to be a message, got {last_event.type}")
+    user_event_data = cast(MessageEventData, last_event.data)
+    for part in user_event_data["parts"]:
+        if part["type"] == "content":
+            user_input = part["content"]
+            break
+    if not user_input:
+        raise ValueError("No TextPart found in user event data")
+
+    return str(user_input)
+
+
+async def send_processing_event(deps: Deps, content: str) -> None:
+    await emit_processing_event(
+        event_emitter=deps.event_emitter,
+        correlation_id=deps.correlator.correlation_id,
+        content=content,
+    )
+
+
+async def emit_processing_event(
+    event_emitter: EventEmitter, correlation_id: str, content: str
+) -> None:
+    await event_emitter.enqueue_status_event(
+        correlation_id=correlation_id,
+        data=StatusEventData(
+            type="status", status="processing", data={STATUS_EVENT_DATA_KEY: content}
+        ),
+    )
